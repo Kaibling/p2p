3ff49a7ba02ce0b5a7b28c5c661669055d6ae2a1
Kaibling <l.rath@gmx.at>
2019-12-08 20:38:54 +0000

    fix test

--- p2pServer/p2pServer_test.go
+++ p2pServer/p2pServer_test.go
@@ -1,41 +1,11 @@
 package p2pServer
 
 import (
-	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"github.com/Kaibling/p2p/libs/Node"
-	"github.com/Kaibling/p2p/libs/util"
 )
-
-func TestGetNodesHandler(t *testing.T) {
-	res := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "http://localhost:54321/getNodes", nil)
-
-	config := &util.Configuration{
-		BindingIPAddress: "1.2.3.4",
-		BindingPort:      "54321",
-		PeerServer:       ""}
-	testNode := Node.NewNode("1.2.3.4", "54321")
-	server := Newp2pServer(config)
-
-	server.getNodesHandler(res, req)
-
-	var result []Node.Node
-	json.NewDecoder(res.Body).Decode(&result)
-
-	if result[0].IPaddress != testNode.IPaddress {
-		t.Errorf("Expected %s, got %s", result[0].IPaddress, testNode.IPaddress)
-	}
-
-	if result[0].Port != testNode.Port {
-		t.Errorf("Expected %s, got %s", result[0].Port, testNode.Port)
-	}
-
-}
-
 func TestPingHandler(t *testing.T) {
 	res := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "http://localhost:54321/ping", nil)
